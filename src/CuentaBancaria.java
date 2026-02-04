520c9911c740cc0dcac085a1266174cbe8547312
ana <ana.tique1@estudiantesunibague.edu.co>
2026-02-04 09:51:37 -0500

    metodos de clase cuenta bancaria

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class CuentaBancaria {
+    private final String id;
+    private int saldo;
+
+    public CuentaBancaria(String id, int saldoInicial){
+        this.id = id;
+        this.saldo = saldoInicial;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void consignar(int valorAConsignar) {
+        if (valorAConsignar <= 0) {
+            throw new IllegalArgumentException("El valor a consignar no es valido");
+        }
+
+        this.saldo += valorAConsignar;
+    }
+    public void retirar(int valorARetirar) {
+        if (valorARetirar <= 0) {
+            throw new IllegalArgumentException("El valor a Retirar no es valido");
+        }
+        if (valorARetirar > saldo) {
+            throw new IllegalArgumentException("no se puede Retirar de más");
+        }
+
+        this.saldo -= valorARetirar;
+    }
 }
